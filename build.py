3cb0c251ed8d2ed271239f44234fc7d393580ef7
Uilian Ries <uilianries@gmail.com>
2017-07-20 03:30:54 +0000

    Build CppRestSDK on Linux
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ build.py
@@ -0,0 +1,8 @@
+"""This script build Conan.io package to multiple platforms."""
+from conan.packager import ConanMultiPackager
+
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager()
+    builder.add_common_builds(shared_option_name="cpprestsdk:shared", pure_c=False)
+    builder.run()
---
+++ conanfile.py
@@ -0,0 +1,47 @@
+"""Conan.io recipe for CppRestSDK library
+"""
+from conans import ConanFile, CMake, tools
+from os import path
+
+class CppRestSDKConan(ConanFile):
+    """Checkout CppRestSDK, build and create package
+    """
+    name = "cpprestsdk"
+    version = "2.9.1"
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False]}
+    exports_sources = "CMakeLists.txt"
+    url = "https://github.com/Microsoft/cpprestsdk"
+    author = "Uilian Ries <uilianries@gmail.com>"
+    description = "A project for cloud-based client-server communication in native code using a modern asynchronous C++ API design"
+    license = "https://github.com/Microsoft/cpprestsdk/blob/master/license.txt"
+    requires = "Boost/1.62.0@lasote/stable", "OpenSSL/1.0.2l@conan/stable"
+    default_options = "shared=True", "Boost:fPIC=True"
+    cpprestsdk_dir = "%s-%s" % (name, version)
+
+    def source(self):
+        self.run("git clone --branch v%s %s.git %s" % (self.version, self.url, self.cpprestsdk_dir))
+
+    def build(self):
+        cmake = CMake(self)
+        # TODO (uilianries): Use scope to solve tests https://conanio.readthedocs.io/en/latest/mastering/scopes.html
+        cmake.definitions["BUILD_TESTS"] = False
+        cmake.definitions["BUILD_SAMPLES"] = False
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        release_dir = path.join(self.cpprestsdk_dir, "Release")
+        self.copy("license.txt",  dst=".", src=self.cpprestsdk_dir)
+        self.copy(pattern="*.h", dst="include", src=path.join(release_dir, "include"))
+        self.copy(pattern="*.hpp", dst="include", src=path.join(release_dir, "include"))
+        self.copy(pattern="*.dat", dst="include", src=path.join(release_dir, "include"))
+        self.copy(pattern="*.so*", dst="lib", src=path.join(release_dir, "Binaries"), keep_path=False)
+        self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs.append("cpprest")
