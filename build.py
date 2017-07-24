c7f866ca3b311a77b587679d4fec1415df9a6dfe
Uilian Ries <uilianries@gmail.com>
2017-07-24 19:51:57 -0300

    Build Clang with libc++
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,8 +1,16 @@
 """This script build Conan.io package to multiple platforms."""
 from conan.packager import ConanMultiPackager
+from copy import copy
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build missing")
     builder.add_common_builds(shared_option_name="cpprestsdk:shared", pure_c=False)
+    # For Linux + Clang use libc++
+    if settings["compiler"] == "clang" and settings["os"] == "Linux":
+        clang_builds = []
+        for settings, options, env_vars, build_requires in builder.builds:
+            settings["libcxx"] = "libc++"
+            clang_builds.append([settings, options, env_vars, build_requires])
+        builder.builds = clang_builds
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,9 @@
 """
 from conans import ConanFile, CMake, tools
 from os import path
+from tempfile import mkdtemp
 import subprocess
+
 
 class CppRestSDKConan(ConanFile):
     """Checkout CppRestSDK, build and create package
@@ -20,6 +22,7 @@ class CppRestSDKConan(ConanFile):
     requires = "Boost/1.62.0@lasote/stable", "OpenSSL/1.0.2l@conan/stable"
     default_options = "shared=True"
     cpprestsdk_dir = "%s-%s" % (name, version)
+    install_dir = mkdtemp()
 
     def source(self):
         self.run("git clone --depth=50 --branch=v%s %s.git %s" % (self.version, self.url, self.cpprestsdk_dir))
@@ -30,33 +33,17 @@ class CppRestSDKConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["CMAKE_INSTALL_PREFIX"] = self.install_dir
         cmake.definitions["BUILD_TESTS"] = True if self.scope.dev and self.scope.build_tests else False
         cmake.definitions["BUILD_SAMPLES"] = True if self.scope.dev and self.scope.build_samples else False
-        if cmake.definitions["BUILD_TESTS"] and self.settings.os == "Linux":
-            self._insert_pthread()
         cmake.configure()
         cmake.build()
-
-    def _insert_pthread(self):
-        # test_runner does not find pthread_create
-        old_line = "        -Wl,--whole-archive"
-        new_line = """        -Wl,--whole-archive
-        pthread"""
-        cmake_path = path.join(self.cpprestsdk_dir, "Release", "tests", "common", "TestRunner", "CMakeLists.txt")
-        tools.replace_in_file(cmake_path, old_line, new_line)
-
+        cmake.install()
 
     def package(self):
-        release_dir = path.join(self.cpprestsdk_dir, "Release")
         self.copy("license.txt",  dst=".", src=self.cpprestsdk_dir)
-        self.copy(pattern="*.h", dst="include", src=path.join(release_dir, "include"))
-        self.copy(pattern="*.hpp", dst="include", src=path.join(release_dir, "include"))
-        self.copy(pattern="*.dat", dst="include", src=path.join(release_dir, "include"))
-        self.copy(pattern="*.so*", dst="lib", src=path.join(release_dir, "Binaries"), keep_path=False)
-        self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
-        self.copy(pattern="*.dylib*", dst="lib", src=path.join(release_dir, "Binaries"), keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
-        self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
+        self.copy(pattern="*", dst="include", src=path.join(self.install_dir, "include"))
+        self.copy(pattern="*", dst="lib", src=path.join(self.install_dir, "lib"), keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs.append("cpprest")
