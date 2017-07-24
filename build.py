fd47941e62d91c26b36a2743f686e9765359f1de
Uilian Ries <uilianries@gmail.com>
2017-07-24 20:30:31 -0300

    Fix clang builds
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 """This script build Conan.io package to multiple platforms."""
 from conan.packager import ConanMultiPackager
 from copy import copy
+import platform
 
 
 if __name__ == "__main__":
@@ -9,8 +10,9 @@ if __name__ == "__main__":
     # For Linux + Clang use libc++
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        if settings["compiler"] == "clang" and settings["os"] == "Linux":
-                settings["libcxx"] = "libc++"
+        print(settings)
+        if settings["compiler"] == "clang" and platform.system() == "Linux":
+                settings["compiler.libcxx"] = "libc++"
         filtered_builds.append([settings, options, env_vars, build_requires])
     builder.builds = filtered_builds
     builder.run()
