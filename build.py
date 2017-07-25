6f6c05e0e65ccbbf35ebb4939ba991b7809f4510
Uilian Ries <uilianries@gmail.com>
2017-07-24 22:44:38 -0300

    OSX build pages
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,18 +1,17 @@
 """This script build Conan.io package to multiple platforms."""
 from conan.packager import ConanMultiPackager
-from copy import copy
 import platform
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build missing")
     builder.add_common_builds(shared_option_name="cpprestsdk:shared", pure_c=False)
-    # For Linux + Clang use libc++
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        print(settings)
+        if platform.system() == "Linux":
+            options["Boost:fPIC"] = True
         if settings["compiler"] == "clang" and platform.system() == "Linux":
-                settings["compiler.libcxx"] = "libc++"
+            settings["compiler.libcxx"] = "libc++"
         filtered_builds.append([settings, options, env_vars, build_requires])
     builder.builds = filtered_builds
     builder.run()
