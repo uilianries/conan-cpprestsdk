79a438096d5c0b491316b3db4d328088c51ce77a
Uilian Ries <uilianries@gmail.com>
2017-07-26 23:11:28 -0300

    Change to Boost 1.60.0 Lite
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .travis/run.sh
+++ .travis/run.sh
@@ -11,9 +11,3 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 fi
 
 python build.py
-
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    echo "TODO (uilian.ries): Remove after upload all packages"
-    conan upload -r upload_repo zlib/1.2.8@lasote/stable --all --force || true
-    conan upload -r upload_repo Boost/1.62.0@lasote/stable --all --force || true
-fi
--- conanfile.py
+++ conanfile.py
@@ -2,6 +2,7 @@
 """
 from conans import ConanFile, CMake
 from conans.model.options import OptionsValues
+from conans.model.requires import Requirements
 from os import path
 from tempfile import mkdtemp
 
@@ -19,10 +20,29 @@ class CppRestSDKConan(ConanFile):
     author = "Uilian Ries <uilianries@gmail.com>"
     description = "A project for cloud-based client-server communication in native code using a modern asynchronous C++ API design"
     license = "https://github.com/Microsoft/cpprestsdk/blob/master/license.txt"
-    requires = "Boost/1.62.0@lasote/stable", "OpenSSL/1.0.2l@conan/stable"
-    default_options = "shared=True"
+    requires = "Boost/1.60.0@lasote/stable", "OpenSSL/1.0.2l@conan/stable"
     cpprestsdk_dir = "%s-%s" % (name, version)
     install_dir = mkdtemp()
+    default_options = "shared=True", \
+        "Boost:without_container=True", \
+        "Boost:without_context=True", \
+        "Boost:without_coroutine=True", \
+        "Boost:without_coroutine2=True", \
+        "Boost:without_exception=True", \
+        "Boost:without_graph=True", \
+        "Boost:without_graph_parallel=True", \
+        "Boost:without_iostreams=True", \
+        "Boost:without_locale=True", \
+        "Boost:without_log=True", \
+        "Boost:without_math=True", \
+        "Boost:without_mpi=True", \
+        "Boost:without_program_options=True", \
+        "Boost:without_serialization=True", \
+        "Boost:without_signals=True", \
+        "Boost:without_test=True", \
+        "Boost:without_timer=True", \
+        "Boost:without_type_erasure=True", \
+        "Boost:without_wave=True"
 
     def source(self):
         self.run("git clone --depth=50 --branch=v%s %s.git %s" % (self.version, self.url, self.cpprestsdk_dir))
