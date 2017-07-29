c280431f5aa6d62a96611824ab5ffab428976b62
Uilian Ries <uilianries@gmail.com>
2017-07-29 18:47:26 -0300

    Build on Windows
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -8,9 +8,6 @@ if __name__ == "__main__":
     builder.add_common_builds(shared_option_name="cpprestsdk:shared", pure_c=False)
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        # Avoid to build Macos and static library
-        if options["cpprestsdk:shared"] == False and platform.system() == "Darwin":
-            continue
         # Avoid to build CLang + libstdc++
         if settings["compiler"] == "clang" and platform.system() == "Linux":
             settings["compiler.libcxx"] = "libc++"
